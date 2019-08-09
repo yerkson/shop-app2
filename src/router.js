c36d5f265d11b5ee3e1a0fb3df60bedf9d498765
wangquan <328053660@qq.com>
2019-08-09 17:58:00 +0800

    init project

--- src/router.js
+++ src/router.js
@@ -1,23 +1,53 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
+      path: '/msite',
+      name: 'MSite',
+      component: () => import('./views/MSite/MSite'),
+      meta: {
+        showFooter: true
+      }
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/order',
+      name: 'Order',
+      component: () => import('./views/Order/Order'),
+      meta: {
+        showFooter: true
+      }      
+    },
+    {
+      path: '/profile',
+      name: 'Profile',
+      component: () => import('./views/Profile/Profile'),
+      meta: {
+        showFooter: true
+      }      
+    },
+    {
+      path: '/search',
+      name: 'Search',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () => import('./views/Search/Search'),
+      meta: {
+        showFooter: true
+      }      
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('./views/Login/Login')
     }
   ]
 })
